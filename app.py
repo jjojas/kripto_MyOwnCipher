f012310d94a0d2b5345f2edc4b27ca61a48fbd3d
jjojas <justinjojas@gmail.com>
2022-02-16 20:09:54 +0700

    fix error binary

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMessageBox
 
-import modules.encrypt as e
+import modules.encrypt as encr
 
 from os import environ
 
@@ -23,7 +23,7 @@ class rc4Widget(qtw.QWidget):
     
     def initUI(self):
         def encrypt():
-            cipherCache = e.encryptText(ptextBox.text(),ktextBox.text())
+            cipherCache = encr.encryptText(ptextBox.text(),ktextBox.text())
             # splitted = vig.splitStringTo5Chars(rep)
             ctextBox.setText(cipherCache)
             # options = qtw.QFileDialog.Options()
@@ -33,13 +33,13 @@ class rc4Widget(qtw.QWidget):
             #     print(fileName)
 
         def decrypt():
-            c = e.decryptText(ctextBox.text(),ktextBox.text())
+            c = encr.decryptText(ctextBox.text(),ktextBox.text())
             rep = repr(c)
             # splitted = vig.splitStringTo5Chars(rep)
             ptextBox.setText(rep[:-1][1:])
 
         def save():
-            e.saveCipherToTextfile(ctextBox.text(),saveLine.text())
+            encr.saveCipherToTextfile(ctextBox.text(),saveLine.text())
             msg = QMessageBox()
             msg.setText("File tersimpan!")
             msg.setInformativeText(f'Cipherteks berhasil disimpan pada direktori cipher/text/{saveLine.text()}.txt')
@@ -52,14 +52,14 @@ class rc4Widget(qtw.QWidget):
                 options |= qtw.QFileDialog.DontUseNativeDialog
                 fileName, _ = qtw.QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*)", options=options)
                 if fileName:
-                    try:
-                        e.encryptBinaryFile(fileName,ktextBox.text())
-                        msg = QMessageBox()
-                        msg.setText(f"Filemu berhasil dienkripsi di cipher/files/encrypted_{fileName.split('/')[-1]}")
-                        msg.setWindowTitle("Enkripsi Berhasil")
-                        msg.exec_()
-                    except Exception as e:
-                        print(e)
+                    # try:
+                    encr.encryptBinaryFile(fileName,ktextBox.text())
+                    msg = QMessageBox()
+                    msg.setText(f"Filemu berhasil dienkripsi di cipher/files/encrypted_{fileName.split('/')[-1]}")
+                    msg.setWindowTitle("Enkripsi Berhasil")
+                    msg.exec_()
+                    # except Exception as e:
+                    #     print(e)
             else:
                 msg = QMessageBox()
                 msg.setText("Cipher key tidak boleh kosong!")
@@ -73,7 +73,7 @@ class rc4Widget(qtw.QWidget):
                 fileName, _ = qtw.QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*)", options=options)
                 if fileName:
                     try:
-                        e.decryptBinaryFile(fileName,ktextBox.text())
+                        encr.decryptBinaryFile(fileName,ktextBox.text())
                         msg = QMessageBox()
                         msg.setText(f"Filemu berhasil dienkripsi di cipher/files/decrypted_{fileName.split('/')[-1]}")
                         msg.setWindowTitle("Enkripsi Berhasil")
